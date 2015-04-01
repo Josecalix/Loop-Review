06d57f1994fa5620afe29f6d2e13117df3701fb7
Jose Calix <jbcalix@hotmail.com>
2015-03-31 21:26:04 -0400

    initial commit

---
+++ EvenNumbers.java
@@ -0,0 +1,17 @@
+ 
+ public class EvenNumbers {
+
+	public static void main(String[] args) {
+		int start, evenNumber;
+		
+		start = 1;
+		do 
+		{ evenNumber = 2*start;
+		  start ++;	
+		  System.out.println(evenNumber);
+	    } while (start < 200 );
+		
+	}
+
+}
+
